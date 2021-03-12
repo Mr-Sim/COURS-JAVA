572d1cc316106cade60ce45880496eda74b52085
Simon Catanese <simoncatanese.pro@gmail.com>
2021-03-12 16:27:15 +0100

    commencement du TP 2

---
+++ TP2/src/com/example/TP2/Longueur.java
@@ -0,0 +1,9 @@
+package com.example.TP2;
+
+import java.util.Scanner
+
+public class Longueur {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ TP2/src/com/example/TP2/Verbes.java
@@ -0,0 +1,28 @@
+package com.example.TP2;
+
+import java.util.Scanner;
+import java.lang.Object;
+
+public class Verbes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nbChar = 2;
+        String groupe = " n'est pas un verbe";
+        System.out.println("Ecrivez un verbe à l'infinitif :");
+        String intVerb = sc.next();
+
+        if(intVerb.endsWith("er"))
+            groupe = " est un verbe de premier groupe.";
+
+        else if(intVerb.endsWith("ir")) {
+            groupe = " est un verbe de deuxième groupe.";
+            if(intVerb.endsWith("oir"))
+                groupe = " est un verbe de troisième groupe";
+        }
+        else if(intVerb.endsWith("oire")) {
+            groupe = " est un verbe de troisième groupe.";
+        }
+        System.out.println(intVerb+groupe);
+    }
+}
+
